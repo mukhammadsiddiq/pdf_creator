82a3884528c12e3a1ce529a76e58a255b8ab87dc
muxammad <ibrohimovmuhammad2020@gmail.com>
2023-12-07 13:26:35 +0100

    adding more pages to the pdf

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ df = pd.read_csv("topics.csv")
 
 
 for index, row in df.iterrows():
-    pdf.add_page()
-    pdf.set_font(family="Times", size=12, style="B")
-    pdf.set_text_color(100, 100, 100)
-    pdf.cell(w=0, h=12, txt=row["Topic"], align="l", ln=1)
-    pdf.line(10,21,200,21)
+    for i in range(row["Pages"]):
+        pdf.add_page()
+        pdf.set_font(family="Times", size=12, style="B")
+        pdf.cell(w=0, h=12, txt=row["Topic"], align="l", ln=1)
+        pdf.line(10, 21, 200, 21)
 pdf.output("output.pdf")
